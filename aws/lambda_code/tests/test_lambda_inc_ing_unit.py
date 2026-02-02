7bdb5afbfe45d4ccb6f5feea68a8a05d885f5542
AllaKop <allchenok020894@gmail.com>
2026-02-02 17:37:02 +0200

    changed import

--- aws/lambda_code/tests/test_lambda_inc_ing_unit.py
+++ aws/lambda_code/tests/test_lambda_inc_ing_unit.py
@@ -1,9 +1,9 @@
-import aws.lambda_code.nbu_exchange_api_incr_ingestion_s3
+from aws.lambda_code import nbu_exchange_api_incr_ingestion_s3
 from unittest.mock import patch, MagicMock
 
 def test_build_url():
     date = "20260202"
-    url = aws.lambda_code.nbu_exchange_api_incr_ingestion_s3.build_url(date)
+    url = nbu_exchange_api_incr_ingestion_s3.build_url(date)
     assert "start=20260202" in url
     assert "end=20260202" in url
 
@@ -12,13 +12,13 @@ def test_fetch_data_success(mock_get):
     mock_response = MagicMock()
     mock_response.status_code = 200
     mock_get.return_value = mock_response
-    url = aws.lambda_code.nbu_exchange_api_incr_ingestion_s3.build_url("20260202")
-    response = aws.lambda_code.nbu_exchange_api_incr_ingestion_s3.fetch_data(url)
+    url = nbu_exchange_api_incr_ingestion_s3.build_url("20260202")
+    response = nbu_exchange_api_incr_ingestion_s3.fetch_data(url)
     assert response.status_code == 200
 
 @patch('nbu_exchange_api_incr_ingestion_s3.boto3.client')
 def test_save_to_s3(mock_boto_client):
     mock_s3 = MagicMock()
     mock_boto_client.return_value = mock_s3
-    aws.lambda_code.nbu_exchange_api_incr_ingestion_s3.save_to_s3(mock_s3, "bucket", "key", {"a": 1})
+    nbu_exchange_api_incr_ingestion_s3.save_to_s3(mock_s3, "bucket", "key", {"a": 1})
     mock_s3.put_object.assert_called_once()
